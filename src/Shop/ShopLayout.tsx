16107418d550d2f4c07bd3b9f5e08dfbb088291e
Sam Hunt <samhunt.dev@gmail.com>
2025-07-04 22:40:34 +1200

    Refactor: App bar nav buttons to links

--- src/Shop/ShopLayout.tsx
+++ src/Shop/ShopLayout.tsx
@@ -1,12 +1,11 @@
 import HandymanIcon from "@mui/icons-material/Handyman";
 import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
 import GitHubIcon from '@mui/icons-material/GitHub';
-import { AppBar, Badge, Box, Container, IconButton, Stack, Toolbar, Tooltip, Typography } from "@mui/material"
-import { Outlet, useNavigate } from "react-router"
+import { AppBar, Badge, Box, Container, IconButton, Link, Stack, Toolbar, Tooltip, Typography } from "@mui/material"
+import { Outlet } from "react-router"
 import { useCart } from "./api/use-cart";
 
 export const ShopLayout = () => {
-  const navigate = useNavigate();
 
   const { cart } = useCart();
   const cartItemCount = cart?.reduce((sum, item) => sum + item.amount, 0);
@@ -14,7 +13,7 @@ export const ShopLayout = () => {
   return <>
     <AppBar position="sticky">
       <Toolbar sx={{ justifyContent: 'space-between' }}>
-        <Stack direction="row" onClick={() => navigate('/')} alignItems="center" spacing={1} sx={{ cursor: 'pointer' }}>
+        <Stack direction="row" component={Link} href="/" color="inherit" alignItems="center" spacing={1} sx={{ cursor: 'pointer', textDecoration: 'none' }}>
           <HandymanIcon />
           <Typography variant="h6" component="div">
             Hardware Store
@@ -28,7 +27,9 @@ export const ShopLayout = () => {
             color="inherit"
             aria-label="open on github"
             sx={{ mr: 2 }}
-            onClick={() => window.open('https://github.com/sam-hunt/hard-net', '_blank')}
+            LinkComponent="a"
+            href="https://github.com/sam-hunt/hard-net"
+            target="_blank"
           >
             <GitHubIcon />
           </IconButton>
@@ -40,7 +41,7 @@ export const ShopLayout = () => {
             color="inherit"
             aria-label="view cart"
             sx={{ mr: 2 }}
-            onClick={() => navigate('/cart')}
+            href="/cart"
           >
             <Badge badgeContent={cartItemCount} color="error">
               <ShoppingCartIcon />
