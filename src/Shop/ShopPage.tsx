b6ee563aef6b8a0bf8a6a07627054b95c2759500
Sam Hunt <samhunt.dev@gmail.com>
2025-07-04 23:13:44 +1200

    feat: Add loading skeletons to product cards

--- src/Shop/ShopPage.tsx
+++ src/Shop/ShopPage.tsx
@@ -1,20 +1,28 @@
 import { useQuery } from "@tanstack/react-query"
 import { fetchProducts } from "./api/api"
-import { Grid, Typography } from "@mui/material";
+import { Grid, Skeleton, Typography } from "@mui/material";
 import { ProductCard } from "./ProductCard";
 
 export const ShopPage = () => {
-  const productsQuery = useQuery({ queryKey: ['products'], queryFn: fetchProducts, });
+  const { data: products, isLoading } = useQuery({ queryKey: ['products'], queryFn: fetchProducts, });
 
   return (
     <>
       <Typography variant="h2" component="h1" my={3}>Products</Typography>
       <Grid container spacing={2} pt={2}>
-        {productsQuery.data?.map(product => (
-          <Grid key={product.name} size={{ xs: 12, sm: 6, lg: 4 }}>
-            <ProductCard product={product} />
-          </Grid>
-        ))}
+        {isLoading
+          ? [0.5, 0.4, 0.3, 0.2, 0.1].map(opacity => (
+            <Grid key={opacity.toString()} size={{ xs: 12, sm: 6, lg: 4 }} sx={{ opacity }}>
+              <Skeleton variant="rounded" height={294} width="100%">
+                <ProductCard product={{ name: 'skeleton', price: 0 }} />
+              </Skeleton>
+            </Grid>
+          ))
+          : products?.map(product => (
+            <Grid key={product.name} size={{ xs: 12, sm: 6, lg: 4 }}>
+              <ProductCard product={product} />
+            </Grid>
+          ))}
       </Grid>
     </>
   );
